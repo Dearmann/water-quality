d366d7fc510444aeb6352af2725276e3115b876a
Dearmann <duszak0@gmail.com>
2022-01-23 00:53:12 +0100

    Added AJAX sensory data refresh

--- esp8266_server/esp8266_server.ino
+++ esp8266_server/esp8266_server.ino
@@ -4,24 +4,22 @@
 
 ESP8266WebServer server(80);
 
-const char* ssid = "ssid"; //Enter Wi-Fi SSID
-const char* password =  "passwd"; //Enter Wi-Fi Password
- 
+const char* ssid = "ssid";
+const char* password =  "password";
+float temp = 0, light = 0, ph = 0, tss = 0, tds = 0;
+
 void setup() {
-  Serial.begin(9600); //Begin Serial at 115200 Baud
-  WiFi.begin(ssid, password);  //Connect to the WiFi network
-  
-  while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
       delay(500);
       Serial.println("Waiting to connect...");
   }
-  
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  //Print the local IP
-  
+  Serial.println(WiFi.localIP());
   server.on("/", handle_index); //Handle Index page
-  
-  server.begin(); //Start the server
+  server.on("/update", update_data); //Handle AJAX calls
+  server.begin();
   Serial.println("Server listening");
 }
 
@@ -29,42 +27,104 @@ void loop() {
   server.handleClient(); //Handling of incoming client requests
 }
 
-void handle_index() {
-    // Send a JSON-formatted request with key "type" and value "request"
-  // then parse the JSON-formatted response with keys "gas" and "distance"
-  DynamicJsonDocument doc(512);
-  float temp = 0, light = 0, ph = 0, tss = 0, tds = 0;
-//  // Sending the request
+void update_data() {
+  DynamicJsonDocument doc(128);
+
+  // Sending request
   doc["type"] = "request";
   serializeJson(doc,Serial);
-//  // Reading the response
+
+  // Reading response
   boolean messageReady = false;
   String message = "";
-  while(messageReady == false) { // blocking but that's ok
+  while(messageReady == false) {
     if(Serial.available()) {
       message = Serial.readString();
       messageReady = true;
     }
   }
-//  // Attempt to deserialize the JSON-formatted message
   DeserializationError error = deserializeJson(doc,message);
   if(error) {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.c_str());
-    return;
+//    return;
   }
+  
+  // Prepare HTTP response
   temp = doc["temp"];
   light = doc["light"];
   ph = doc["ph"];
   tss = doc["tss"];
   tds = doc["tds"];
-  // Prepare the data for serving it over HTTP
-  String output = "Temerature: " + String(temp) + "\n";
-  output += "Light: " + String(light) + "\n";
-  output += "pH: " + String(ph) + "\n";
-  output += "TSS: " + String(tss) + "\n";
-  output += "TDS: " + String(tds);
-  // Serve the data as plain text, for example
-  server.send(200,"text/plain",output);
+  String output = "<h2>Temperature: " + String(temp) + " C</h2>";
+  output += "<h2>Light: " + String(light) + " lx</h2>";
+  output += "<h2>pH: " + String(ph) + "</h2>";
+  output += "<h2>TSS: " + String(tss) + " NTU</h2>";
+  output += "<h2>TDS: " + String(tds) + " ppm</h2>";
+  server.send(200,"text/html",output);
+  Serial.readString();
+}
+
+void handle_index() {
+  DynamicJsonDocument doc(128);
+
+  // Sending request
+  doc["type"] = "request";
+  serializeJson(doc,Serial);
+
+  // Reading response
+  boolean messageReady = false;
+  String message = "";
+  while(messageReady == false) {
+    if(Serial.available()) {
+      message = Serial.readString();
+      messageReady = true;
+    }
+  }
+  DeserializationError error = deserializeJson(doc,message);
+  if(error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+//    return;
+  }
+
+  // Prepare HTTP response
+  temp = doc["temp"];
+  light = doc["light"];
+  ph = doc["ph"];
+  tss = doc["tss"];
+  tds = doc["tds"];
+
+  String output = "<!DOCTYPE html>";
+  output += "<html lang=\"en\">";
+  output += "<head>";
+      output += "<meta charset=\"UTF-8\">";
+      output += "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">";
+      output += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+      output += "<title>Water Quality</title>";
+  output += "</head>";
+  output += "<body>";
+    output += "<h1>Water quality:</h1>";
+    output += "<div id=\"data\">";
+      output += "<h2>Temperature: " + String(temp) + " C</h2>";
+      output += "<h2>Light: " + String(light) + " lx</h2>";
+      output += "<h2>pH: " + String(ph) + "</h2>";
+      output += "<h2>TSS: " + String(tss) + " NTU</h2>";
+      output += "<h2>TDS: " + String(tds) + " ppm</h2>";
+    output += "</div>";
+    output += "<script>";
+      output += "setInterval(function () { getData(); }, 2000);";
+      output += "function getData() {";
+        output += "var xhttp = new XMLHttpRequest();";
+        output += "xhttp.onreadystatechange = function () {";
+          output += "if (this.readyState == 4 && this.status == 200) {";
+            output += "document.getElementById(\"data\").innerHTML = this.responseText;}};";
+        output += "xhttp.open(\"GET\", \"update\", true);";
+        output += "xhttp.send();}";
+  output += "</script>";
+  output += "</body>";
+  output += "</html>";
+
+  server.send(200,"text/html",output);
   Serial.readString();
 }
